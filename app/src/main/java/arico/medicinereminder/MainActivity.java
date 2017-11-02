536604e423d7aec14a5f05f1edaad04a66d801de
RicoSuaveDesign <arico@carthage.edu>
2017-11-02 01:49:07 -0500

    Minor progress
    Added a bare-bones notification.

---
+++ app/src/main/java/arico/medicinereminder/MainActivity.java
@@ -0,0 +1,38 @@
+package arico.medicinereminder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.view.View;
+import android.content.Context;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendNotification(View view) {
+
+//Get an instance of NotificationManager//
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.notification_icon)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+
+// Gets an instance of the NotificationManager service//
+
+        NotificationManager mNotificationManager =
+
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+
+    }
+
+}
